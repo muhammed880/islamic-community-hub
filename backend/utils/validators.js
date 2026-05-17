52dacf34d46788c270ccf14bcc7849ddb93ba231
muhammed880 <muhammednali48@gmail.com>
2026-05-17 12:41:15 +0530

    Update validators.js

--- backend/utils/validators.js
+++ backend/utils/validators.js
@@ -1,29 +1,23 @@
-// Email validator
 const isValidEmail = (email) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
 };
 
-// Phone validator
 const isValidPhone = (phone) => {
   const phoneRegex = /^\+?[1-9]\d{1,14}$/;
   return phoneRegex.test(phone);
 };
 
-// UPI ID validator
 const isValidUpiId = (upiId) => {
   const upiRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z]+$/;
   return upiRegex.test(upiId);
 };
 
-// Password validator
 const isStrongPassword = (password) => {
-  // At least 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   return passwordRegex.test(password);
 };
 
-// Amount validator
 const isValidAmount = (amount) => {
   return Number.isFinite(amount) && amount > 0;
 };
